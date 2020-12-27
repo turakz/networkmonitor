e3800acc39e42d57fd545607330fe67e96595f19
turakz <zachary.turak@gmail.com>
2020-12-27 01:28:22 -0500

    added boost 1.74 as dependency

--- conanfile.py
+++ conanfile.py
@@ -7,4 +7,9 @@ class ConanPackage(ConanFile):
     generators = "cmake_find_package"
 
     requires = [
+        ('boost/1.74.0'),
     ]
+
+    default_options = (
+        'boost:shared=False',
+    )
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <boost/asio.hpp>
 
 int main(void)
 {
-	std::cout << "repo structure, initial commit" << std::endl;
-	return 0;
+    boost::system::error_code ec {};
+    if (ec)
+    {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    }
+    else
+    {
+        std::cout << "OK" << std::endl;
+    }
+    return 0;
 }
