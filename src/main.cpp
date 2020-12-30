c1be23d0f89de688ffddc255b1eb229de99a6910
turakz <zachary.turak@gmail.com>
2020-12-30 00:49:41 -0500

    websocket client commit, my attempt

--- src/main.cpp
+++ src/main.cpp
@@ -5,69 +5,17 @@
 #include <iostream>
 #include <string>
 
-using NetworkMonitor::WebSocketClient;
-
 int main()
 {
     // Connection targets
     const std::string url {"echo.websocket.org"};
     const std::string port {"80"};
-    const std::string message {"Hello WebSocket"};
 
     // Always start with an I/O context object.
     boost::asio::io_context ioc {};
 
-    // The class under test
-    WebSocketClient client {url, port, ioc};
-
-    // We use these flags to check that the connection, send, receive functions
-    // work as expected.
-    bool connected {false};
-    bool messageSent {false};
-    bool messageReceived {false};
-    bool messageMatches {false};
-    bool disconnected {false};
-
-    // Our own callbacks
-    auto onSend {[&messageSent](auto ec) {
-        messageSent = !ec;
-    }};
-    auto onConnect {[&client, &connected, &onSend, &message](auto ec) {
-        connected = !ec;
-        if (!ec) {
-            client.Send(message, onSend);
-        }
-    }};
-    auto onClose {[&disconnected](auto ec) {
-        disconnected = !ec;
-    }};
-    auto onReceive {[&client,
-                      &onClose,
-                      &messageReceived,
-                      &messageMatches,
-                      &message](auto ec, auto received) {
-        messageReceived = !ec;
-        messageMatches = message == received;
-        client.Close(onClose);
-    }};
-
-    // We must call io_context::run for asynchronous callbacks to run.
-    client.Connect(onConnect, onReceive);
+    network_monitor::websocket_client client(ioc, url, port);
+    client.connect();
     ioc.run();
-
-    // When we get here, the io_context::run function has run out of work to do.
-    bool ok {
-        connected &&
-        messageSent &&
-        messageReceived &&
-        messageMatches &&
-        disconnected
-    };
-    if (ok) {
-        std::cout << "OK" << std::endl;
-        return 0;
-    } else {
-        std::cerr << "Test failed" << std::endl;
-        return 1;
-    }
+    return 0;
 }
