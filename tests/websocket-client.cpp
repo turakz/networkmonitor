19aff9bf901518246a5b6263b1cfb0b64b1fc3be
turakz <zachary.turak@gmail.com>
2021-01-05 12:28:54 -0500

    integrated std::filesystem, cacert.pem

--- tests/websocket-client.cpp
+++ tests/websocket-client.cpp
@@ -3,11 +3,17 @@
 #include <boost/asio.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include <filesystem>
 #include <string>
 
 using NetworkMonitor::WebSocketClient;
 
 BOOST_AUTO_TEST_SUITE(network_monitor);
+
+BOOST_AUTO_TEST_CASE(cacert_pem)
+{
+    BOOST_CHECK(std::filesystem::exists(TESTS_CACERT_PEM));
+}
 
 BOOST_AUTO_TEST_CASE(class_WebSocketClient)
 {
